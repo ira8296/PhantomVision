ef3cb8cae3ed5d60e3093ede09185a61b50a3e39
Ike Ajah (RIT Student) <ira8296@rit.edu>
2023-09-21 18:02:13 -0400

    Implemented telekinesis

--- PhantomVision/Assets/Scripts/Player.cs
+++ PhantomVision/Assets/Scripts/Player.cs
@@ -16,6 +16,15 @@ public class Player : MonoBehaviour
     List<GameObject> collectibles; //objects the player can collect
     List<GameObject> movables; //objects the player can telekinetically move
 
+    Vector3 screenPosition;
+    Vector3 worldPosition;
+    Vector3 holdPosition;
+
+    public Camera mainCam;
+    LayerMask ground;
+
+    GameObject heldObj = null;
+
 
 
     // Start is called before the first frame update
@@ -23,26 +32,27 @@ public class Player : MonoBehaviour
     {
         position = transform.position;
         direction = transform.forward;
+        holdPosition = transform.GetChild(0).transform.position; 
         speed = 0.01f;
         tangible = true;
         glow = false;
         floating = false;
 
         //Find and record all interactable items in the environment
-        interactables = new List<GameObject>();
+        /*interactables = new List<GameObject>();
         GameObject[] elements = GameObject.FindGameObjectsWithTag("Interactable");
         foreach (GameObject element in elements)
         {
             interactables.Add(element);
-        }
+        }*/
 
         //Find and record all interactable items in the environment
-        collectibles = new List<GameObject>();
+        /*collectibles = new List<GameObject>();
         GameObject[] pickups = GameObject.FindGameObjectsWithTag("Collectible");
         foreach (GameObject pickup in pickups)
         {
             interactables.Add(pickup);
-        }
+        }*/
 
         //Find and record all movable items in the environment
         movables = new List<GameObject>();
@@ -56,25 +66,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ground = LayerMask.GetMask("Ground");
+
         //Movement input
         if (Input.GetKey(KeyCode.W))
         {
             position.z += speed;
+            holdPosition.z += speed;
             direction.z = 1f;
         }
         if (Input.GetKey(KeyCode.S))
         {
             position.z -= speed;
+            holdPosition.z -= speed;
             direction.z = -1f; ;
         }
         if (Input.GetKey(KeyCode.D))
         {
             position.x += speed;
+            holdPosition.x += speed;
             direction.x = 1f;
         }
         if (Input.GetKey(KeyCode.A))
         {
             position.x -= speed;
+            holdPosition.x -= speed;
             direction.x = -1f;
         }
 
@@ -103,6 +119,7 @@ public class Player : MonoBehaviour
 
         Float();
         Glow();
+        Levitate();
 
         transform.forward = direction;
         transform.position = position;
@@ -133,33 +150,57 @@ public class Player : MonoBehaviour
 
     }*/
 
-    void Float()
+    void Float() //Lets player slowly rise into the air
     {
         if (floating)
         {
             position.y += speed;
+            holdPosition.y += speed;
         }
         else
         {
             while(position.y > 1.5)
             {
-                position.y -= speed;
+                position.y -= (speed * -10);
+                holdPosition.y -= (speed * -10);
             }
         }
 
     }
 
-    void Levitate()
+    void Levitate() //Allows player to lift up movable objects they click on
     {
+        //Mouse position logic
+        screenPosition = Input.mousePosition;
 
+        Ray ray = mainCam.ScreenPointToRay(screenPosition);
+        RaycastHit hitData;
+
+        if (Physics.Raycast(ray, out hitData, Mathf.Infinity) && Input.GetMouseButton(0))
+        {
+            worldPosition = hitData.point;
+
+            foreach (GameObject m in movables)
+            {
+                if (m == hitData.transform.gameObject || m.transform.position == holdPosition)
+                {
+                    heldObj = m;
+                }
+
+            }
+        }
+        else
+        {
+            heldObj = null;
+        }
+
+        if(heldObj != null)
+        {
+            heldObj.transform.position = holdPosition;
+        }
     }
 
-    void Phase()
-    {
-
-    }
-
-    void Glow()
+    void Glow() //Allows player to illuminate themselves
     {
         if (glow)
         {
