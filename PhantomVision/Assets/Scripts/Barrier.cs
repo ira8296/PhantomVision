67ded90a9c55746e3d83b455e1512303aa9f70a2
Ike Ajah (RIT Student) <ira8296@rit.edu>
2023-11-13 18:12:20 -0500

    Implementing barrier

--- PhantomVision/Assets/Scripts/Barrier.cs
+++ PhantomVision/Assets/Scripts/Barrier.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a572ae9c82bb16063adde5dd8d4ea74505cda57d5394eca63edb41737878f96e
-size 992
+oid sha256:da12c25daecab212cbec8f0d9150f863795ebf5d8d444a67e5ad94a5f97e6a9b
+size 784
---
+++ PhantomVision/Assets/Scripts/BarrierTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d714a0dfa39a6369c83da22bbc4648f3fe726a3fbaa3240618290cb4aed673ce
+size 999
