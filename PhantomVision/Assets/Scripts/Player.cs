d7a11632e913369cf674a03f112f07c2bff9de34
Ike Ajah (RIT Student) <ira8296@rit.edu>
2023-11-28 20:43:19 -0500

    Constructed chamber

--- PhantomVision/Assets/Scripts/Player.cs
+++ PhantomVision/Assets/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4fee22c5740f290dc1619b022cf4f2fc99554d4ed65998098e7c8efa53c516c
-size 13780
+oid sha256:9a853a7f832b66a1cabc5cba3f640ca23381af7f1f11f56d439135fe331da4f7
+size 13863
