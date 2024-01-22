196659efa4d938fd048da8c35c30c795be80f8d2
Ike Ajah (RIT Student) <ira8296@rit.edu>
2024-01-22 16:12:52 -0500

    Still working on Equip

--- PhantomVision/Assets/Scripts/MCAI.cs
+++ PhantomVision/Assets/Scripts/MCAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37a9683b8a89b18722b9f0de31d2c9c7001da54b5669dd9b4d8db0aa52cdebe4
-size 8672
+oid sha256:ae413bb4ec8b1dcfc16394f5ac66d05938d7a1bc94ab08fdacbb725c0dea7af4
+size 8855
