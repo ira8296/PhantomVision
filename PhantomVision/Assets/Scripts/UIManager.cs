dd63bf18aa3db0fbc243394fc3b7884786b22ff3
Ike Ajah (RIT Student) <ira8296@rit.edu>
2024-03-28 16:50:18 -0400

    Added health bars

--- PhantomVision/Assets/Scripts/UIManager.cs
+++ PhantomVision/Assets/Scripts/UIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8595e392afe0a6b1a1ccc7cdde19c8101edd19277c1c338303b909c67fcb99b7
-size 2595
+oid sha256:3baeb79bd534fc30066509a596c9c8a7d592fef51936fca1160d301f9fca8e90
+size 4249
