a167016087c2d0adec38cc4516be872fb5e00d51
Ike Ajah (RIT Student) <ira8296@rit.edu>
2024-04-04 22:24:59 -0400

    Implementing pause screen

--- PhantomVision/Assets/Scripts/SceneManager.cs
+++ PhantomVision/Assets/Scripts/SceneManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:912e7180c91dd90218a666880f1d88021c415c51ff355be562b82e05ffe08653
-size 2558
+oid sha256:542174ba0f946424db8379fcb481b59783b3dc8bed3691cceeaf8ce909c6dc0f
+size 4030
