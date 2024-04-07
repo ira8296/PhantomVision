ca6bbef4d22240b868c0baf42369faae9dbde085
Ike Ajah (RIT Student) <ira8296@rit.edu>
2024-04-06 23:07:06 -0400

    Fully implemented pause feature

--- PhantomVision/Assets/Scripts/SceneManager.cs
+++ PhantomVision/Assets/Scripts/SceneManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:542174ba0f946424db8379fcb481b59783b3dc8bed3691cceeaf8ce909c6dc0f
-size 4030
+oid sha256:1b80a3832e9d19d6ac6f5a8fab9b51ff7889383b248599ef8e3e945b100f69d3
+size 6899
--- PhantomVision/Assets/Scripts/SpriteBillboard.cs
+++ PhantomVision/Assets/Scripts/SpriteBillboard.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8281eff704ae559e87511ebacc09a7e3721e75c9e0bb2007b9c373b98f257e8d
-size 458
+oid sha256:1e199049c193b05f81f7b73c2190f2f5034f52079ff3b90bf42912fb5163f963
+size 545
