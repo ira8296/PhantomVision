c1d35aef151182f31bb9574f410ad9b5b994d2a3
Ike Ajah (RIT Student) <ira8296@rit.edu>
2024-04-12 18:52:06 -0400

    Animated title scene

--- PhantomVision/Assets/Scripts/SceneManager.cs
+++ PhantomVision/Assets/Scripts/SceneManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6d181a7f46aff41a203f0ccaae5cf08d7ab9807a3ae3bf0311fb394a5ca4fca
-size 7591
+oid sha256:c41dd151923a67cb2bbf225bba2c60efaa89f04ad0bd7c1b7faa28fd1e6c5db9
+size 8379
