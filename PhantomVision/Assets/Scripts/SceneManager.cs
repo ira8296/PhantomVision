ac39170e96c9d5c015fb88b134770f1a43b25325
Ike Ajah (RIT Student) <ira8296@rit.edu>
2024-04-14 15:35:00 -0400

    Further animated title UI

--- PhantomVision/Assets/Scripts/SceneManager.cs
+++ PhantomVision/Assets/Scripts/SceneManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c41dd151923a67cb2bbf225bba2c60efaa89f04ad0bd7c1b7faa28fd1e6c5db9
-size 8379
+oid sha256:98ab45fb67532a1221af20e8c5a620460c6a30abf0fe29c9e8f13d7b58d84b08
+size 9475
