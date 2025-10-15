0af6184d6add4dd30b4022ef2d33bc71b5257b5e
ira8296 <42749209+ira8296@users.noreply.github.com>
2025-10-15 18:59:54 -0400

    Fixed scene transition between Title and Prologue scenes

--- PhantomVision/Assets/Scripts/SceneManager.cs
+++ PhantomVision/Assets/Scripts/SceneManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c804a35d34d9aa6530248dfae69d2a1441545cc1ddc608838ea971e6f5ad4780
-size 12340
+oid sha256:9d7cc07418804fee364cd40040818e80ee98ef432c0fa4fd4d26c2c65e2da5b4
+size 13548
--- PhantomVision/Assets/Scripts/TutorialManager.cs
+++ PhantomVision/Assets/Scripts/TutorialManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5af91d10df650276ffde586e8b9ec3f54e073cc51f4ee4e4fc447fdcb0ff4acf
-size 15419
+oid sha256:ed575c35d4239268745eefb36d42d2ca018e4674695d2057beb6b5b528d54adb
+size 15421
